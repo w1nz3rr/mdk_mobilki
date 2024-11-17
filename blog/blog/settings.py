e4c73e5d7b025b9f4563c71792c77cf58233cd8f
Ванек <zhurama2016@gmail.com>
2024-11-18 01:01:43 +0300

    create basic API's and serializers

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main_app.apps.MainAppConfig'
+    'main_app.apps.MainAppConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- blog/main_app/views.py
+++ blog/main_app/views.py
@@ -1,3 +1,55 @@
+from django.forms import model_to_dict
+from rest_framework import generics
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+
+class UserAPIList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserAPIView(APIView):
+    def get(self, request):
+        query = User.objects.all()
+        return Response({'users': UserSerializer(query, many=True).data})
+
+    def post(self, request):
+        serializer_for_user = UserSerializer(data=request.data)
+        serializer_for_user.is_valid(raise_exception=True)
+        serializer_for_user.save()
+
+        return Response({'new_user': serializer_for_user.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'Error': 'Method PUT not allowed'})
+
+        try:
+            user_id = User.objects.get(pk=pk)
+        except:
+            return Response({'Error': 'Object does not exists'})
+
+        serializer_for_user = UserSerializer(data=request.data, instance=user_id)
+        serializer_for_user.is_valid(raise_exception=True)
+        serializer_for_user.save()
+
+        return Response({'update_user': serializer_for_user.data})
+
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'Error': 'Method DELETE not allowed'})
+
+        try:
+            user_id = User.objects.get(pk=pk)
+            user_id.delete()
+        except:
+            return Response({'Error': 'Object does not delete'})
+
+        return Response({'delete_user': f'user with id {pk} was deleted'})
