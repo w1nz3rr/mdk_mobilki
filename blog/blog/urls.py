70899485623d052626ea960ea0e27738d648f265
Ванек <zhurama2016@gmail.com>
2024-11-18 22:50:23 +0300

    create simple_router and ModelViewSet

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from main_app.views import *
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'users', UserViewSet)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users', UserAPIList.as_view()),
-    path('api/users/<int:pk>', UserAPIView.as_view()),
+    path('api/', include(router.urls))
+    # path('api/users', UserViewSet.as_view({'get': 'list'})),
+    # path('api/users/<int:pk>', UserViewSet.as_view({'put': 'update'})),
+    # path('api/usersdetail/<int:pk>', UserDetailAPIView.as_view()),
 ]
--- blog/main_app/views.py
+++ blog/main_app/views.py
@@ -1,5 +1,5 @@
 from django.forms import model_to_dict
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,48 +8,20 @@ from .serializers import UserSerializer
 
 
 
-class UserAPIList(generics.ListCreateAPIView):
+# class UserAPIList(generics.ListCreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+#
+# class UserAPIUpdate(generics.UpdateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+# class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-class UserAPIView(APIView):
-    def get(self, request):
-        query = User.objects.all()
-        return Response({'users': UserSerializer(query, many=True).data})
-
-    def post(self, request):
-        serializer_for_user = UserSerializer(data=request.data)
-        serializer_for_user.is_valid(raise_exception=True)
-        serializer_for_user.save()
-
-        return Response({'new_user': serializer_for_user.data})
-
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'Error': 'Method PUT not allowed'})
-
-        try:
-            user_id = User.objects.get(pk=pk)
-        except:
-            return Response({'Error': 'Object does not exists'})
-
-        serializer_for_user = UserSerializer(data=request.data, instance=user_id)
-        serializer_for_user.is_valid(raise_exception=True)
-        serializer_for_user.save()
-
-        return Response({'update_user': serializer_for_user.data})
-
-
-    def delete(self, request, *args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({'Error': 'Method DELETE not allowed'})
-
-        try:
-            user_id = User.objects.get(pk=pk)
-            user_id.delete()
-        except:
-            return Response({'Error': 'Object does not delete'})
-
-        return Response({'delete_user': f'user with id {pk} was deleted'})
